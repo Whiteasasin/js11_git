46ca23779849cebd4398e56a0369614f38a32b65
Whiteasasin <104910993+Whiteasasin@users.noreply.github.com>
2023-03-09 01:21:46 +0200

    hw js 2

---
+++ hw js 2/main.js
@@ -0,0 +1,56 @@
+// let a = 5;
+// let b, c;
+
+// b = (a * 5);
+// b = (c = b/2); //  дз условие
+
+// task 1
+
+// let a = 5;
+// let b, c;
+
+// b = (a * 5);
+// b = c = (b/2);
+// console.log(a, b, c)
+//  Розставте дужки так, щоб код не змінив своєї поведінки (працював так само як і зараз). Поясніть, як і в якому порядку обчислюються вирази та вирази
+
+//task 2
+
+// let age = prompt("how old are you ?")
+// let currentYear = 2023 ;
+// let born = alert ("Your born in " + ( currentYear - age) )
+
+// task 3
+
+// let tempC = prompt("Indicate degrees in Celsius")
+// let tempF = tempC * 1.8 + 32 ;
+// alert ("temp in F is " + tempF)
+
+// task 4
+// let a = prompt("Input first number" );
+// let b =  prompt("Input second number" );
+// let answer = Math.floor ( a / b );
+// alert ("answer is " +  answer);
+
+// task 5
+// const rate = 40.1 ;
+// let dolExch = prompt ("Input your dollars to exchange in uah , and actual rate is 40.1 in our bank ");
+// let uah = dolExch * rate .toFixed(2) ;
+// alert ("your hryvnia after the exchange " + uah);
+
+//task 6
+
+// const red = Number(prompt("input color in 10 system")).toString(16);
+// const green = Number(prompt("input color in 10 system")).toString(16);
+// const blue = Number(prompt("input color in 10 system")).toString(16);
+// alert("your css color is #" + red + green + blue);
+
+// task 7 
+
+// let poverhi = prompt("vvedi kolichestvo poverhiv");//9
+// let apartment = prompt("vvedi kolichestvo kvartir na poverhe");//4
+// let numKvart = prompt("vvedi nomer kvartiry");//118
+// let podyezd = Math.ceil( numKvart / apartment  / poverhi) ;
+// let etag = Math.ceil (numKvart / apartment) % poverhi ;
+// alert ("vash podyezd " + podyezd + " i vash etag " + etag);
+
