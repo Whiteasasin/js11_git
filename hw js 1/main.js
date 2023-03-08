ee6afdd15f5e8a128f3ff43fabda419971cbd72d
Whiteasasin <104910993+Whiteasasin@users.noreply.github.com>
2023-03-09 01:21:05 +0200

    hw 1

---
+++ hw js 1/main.js
@@ -0,0 +1,11 @@
+// let firstDay = 3836; //первый день шаги за 8 часов
+// alert("Вы прошли за первый день " + firstDay + " шагов");
+// let secondDay= 5467; //второй день шаги за 8 часов
+// alert("Вы прошли за второй день " + secondDay + " шагов");
+// let KolStepsDayOneHalf= firstDay /8; //количество шагов в час
+// alert("Кол-во шагов в час за первый день " + KolStepsDayOneHalf);
+// let KolStepsDayTwoHalf = secondDay /8; //количество шагов в час
+// alert("Кол-во шагов в час за второй день " + KolStepsDayTwoHalf);
+// let resultStepsDay = firstDay + secondDay; //кол-во шагов за два дня 
+// alert("Ты прошляпил за 2 дня " + resultStepsDay + " шагов");
+
